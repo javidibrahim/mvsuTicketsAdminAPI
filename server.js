556eb68be82c8f2d8d6d242af0fc0019c755d4f1
Javid Ibrahimov <admin@admin.local>
2024-09-28 20:03:01 -0500

    Fixed server.js again...

--- server.js
+++ server.js
@@ -11,9 +11,6 @@ server.use(cors(corsOptions));
 server.use(cookieParser());
 server.use(bodyParser.json());
 server.use('/', adminRoutes);
-server.use('/admin/venues', venueRoutes);
-server.use('/admin/events', eventRoutes);
-server.use('/admin/sports', sportRoutes);
 
 const PORT = process.env.ADMIN_SERVER_PORT || 8802;
 server.listen(PORT, '0.0.0.0', () => {
